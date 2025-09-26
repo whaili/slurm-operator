f945023cae2493c53370f93807d95de2bf870acc
Skyler Malinowski <skyler@schedmd.com>
2025-09-26 16:49:15 -0400

    fix: logger odd number of arguments passed error
    Changelog: Fixed - Fixed odd number of arguments logger error when
     updating pod conditions.
    Cherry-pick: release-0.4

--- internal/controller/nodeset/nodeset_sync_status.go
+++ internal/controller/nodeset/nodeset_sync_status.go
@@ -248,7 +248,7 @@ func (r *NodeSetReconciler) updateNodeSetPodConditions(
 		}
 		err := r.Status().Patch(ctx, toUpdate, client.StrategicMergeFrom(pod))
 		if err != nil {
-			logger.Error(err, "Error patching pod condition", toUpdate)
+			logger.Error(err, "Error patching pod condition", "pod", klog.KObj(toUpdate))
 			return err
 		}
 	}
